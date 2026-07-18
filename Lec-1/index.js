f3d6e4946543874a5460abb3e33765d7d6ccaa81
Nitesh <Nitesh.Nitesh@telos.com>
2026-07-18 15:42:56 +0530

    Javascript variables

---
+++ Lec-1/index.js
@@ -0,0 +1,25 @@
+// var  name; //variable declaration
+// name = "Nitesh" //value initialization
+
+// let age=25;
+// console.log(age)
+// const country="India";
+
+
+// console.log(name);
+
+// var name = "Hello world";
+// console.log(name);
+// // let age = 26;
+// // console.log(age); //can not re-declare
+
+// age =26;
+// console.log(age);
+
+// // const country= "India";
+// country="USA"; //can not re-assign
+// console.log(country);
+
+const age;
+age=10;
+console.log(age); //can not re-declare
